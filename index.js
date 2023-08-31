005d9b6ef83e7297cb1faaa978f14fbf7d9d38ed
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-08-31 22:16:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ function editDistance(s1, s2) {
   return costs[s2.length];
 }
 
-console.log(similarity('Stack Overflow', 'Stack Ovrflow') * 100);
+console.log(similarity('Stack Overflow', 'Stack Ovrfow') * 100);
